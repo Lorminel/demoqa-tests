13609534379ed857d00d892381bf425a0dfc7b2b
Daria Tsvetkova <lorminel22@gmail.com>
2024-03-20 15:11:41 +0300

    Edited PracticeFormTest.java, added PracticeFormTestWithComments.java

--- src/test/java/tests/PracticeFormWithCommentsTest.java
+++ src/test/java/tests/PracticeFormWithCommentsTest.java
@@ -10,7 +10,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class PracticeFormTest {
+public class PracticeFormWithCommentsTest {
 
     @BeforeAll
     static void beforeAll() {
@@ -32,9 +32,13 @@ public class PracticeFormTest {
         $("#lastName").setValue("Tsvetkova");
         $("#userEmail").setValue("dtsvetkova@qa.com");
         $("#genterWrapper").$(byText("Female")).click();
+//        $("#gender-radio-2").parent().click(); // good
+//        $("label[for=gender-radio-2]").click(); // good
         $("#userNumber").setValue("7338844660");
         $("#dateOfBirthInput").click();
+//        $(".react-datepicker__month-select").selectOptionByValue("7");
         $(".react-datepicker__month-select").selectOption("August");
+//        $(".react-datepicker__year-select").selectOptionByValue(String.valueOf(1994));
         $(".react-datepicker__year-select").selectOption("1994");
         $(".react-datepicker__day--022:not(.react-datepicker__day--outside-month)").click();
         $("#subjectsInput").setValue("Biology").pressEnter();
@@ -43,6 +47,7 @@ public class PracticeFormTest {
         $("#currentAddress").setValue("Some address 1");
         $("#state").click();
         $("#stateCity-wrapper").$(new ByText("Haryana")).click();
+//        $("#react-select-3-option-2").click();
         $("#city").click();
         $("#stateCity-wrapper").$(new ByText("Panipat")).click();
         $("#submit").click();
@@ -50,9 +55,19 @@ public class PracticeFormTest {
         $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
         $(".modal-body").shouldHave(text(userName), text("Tsvetkova"), text("dtsvetkova@qa.com")
-                , text("Female"), text("7338844660"), text("22 August,1994"), text("Biology")
-                , text("Reading"), text("img.jpg"), text("Some address 1"), text("Haryana Panipat"));
-
+                                ,text("Female"), text("7338844660"), text("22 August,1994"), text("Biology")
+                                ,text("Reading"), text("img.jpg"), text("Some address 1"), text("Haryana Panipat"));
+        
+//        $$("tr").get(1).shouldHave(text("Daria Tsvetkova"));
+//        $$("tr").get(2).shouldHave(text("dtsvetkova@qa.com"));
+//        $$("tr").get(3).shouldHave(text("Female"));
+//        $$("tr").get(4).shouldHave(text("7338844660"));
+//        $$("tr").get(5).shouldHave(text("22 August,1994"));
+//        $$("tr").get(6).shouldHave(text("Biology"));
+//        $$("tr").get(7).shouldHave(text("Reading"));
+//        $$("tr").get(8).shouldHave(text("img.jpg"));
+//        $$("tr").get(9).shouldHave(text("Some address 1"));
+//        $$("tr").get(10).shouldHave(text("Haryana Panipat"));
     }
 
 }

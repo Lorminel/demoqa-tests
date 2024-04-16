81e46b72e6fb2d8cd48519004c4174707fa1bee0
Daria Tsvetkova <lorminel22@gmail.com>
2024-04-16 18:23:58 +0300

    Edited properties

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -28,8 +28,8 @@ public class TestBase {
         Configuration.browserSize = properties.getBrowser_size();
         Configuration.browserVersion = properties.getBrowser_version();
         Configuration.pageLoadStrategy = "eager";
-//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        Configuration.remote = properties.getRwdriver();
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//        Configuration.remote = properties.getRwdriver();
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(

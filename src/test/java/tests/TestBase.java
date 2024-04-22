b40022f9ccc3c18d7ec86b89fb712860792d0bde
Daria Tsvetkova <lorminel22@gmail.com>
2024-04-22 15:20:36 +0300

    Edited TestBase.java

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,7 +3,6 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import com.codeborne.selenide.webdriver.DriverFactory;
 import helpers.Attach;
 import io.qameta.allure.Step;
 import io.qameta.allure.selenide.AllureSelenide;
@@ -17,21 +16,16 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
-    static String browser = System.getProperty("browser", "firefox");
-   static String browser_version = System.getProperty("browser_version", "122.0");
-   static String browser_size = System.getProperty("browser_Size", "1920x1080");
-   static String remoteURL = System.getProperty("remoteURL",
-           "selenoid.autotests.cloud");
-
-    @Step("Открыть страницу https://demoqa.com в разрешении 1920x1080")
+    @Step("Открыть страницу https://demoqa.com")
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = browser;
-        Configuration.browserSize = browser_size;
-        Configuration.browserVersion = browser_version;
+        Configuration.browser = System.getProperty("browser", "firefox");
+        Configuration.browserSize = System.getProperty("browser_Size", "1920x1080");
+        Configuration.browserVersion = System.getProperty("browser_version", "122.0");
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://user1:1234@" + remoteURL + "/wd/hub";
+        Configuration.remote = "https://user1:1234@" + System.getProperty("remote_URL",
+                "selenoid.autotests.cloud") + "/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
@@ -42,7 +36,7 @@ public class TestBase {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
     }
-
+    @Step("Закрыть страницу, сохранить видео, скриншот и логи браузера")
     @AfterEach
     void afterEach() {
         Attach.screenshotAs("Last screenshot");
